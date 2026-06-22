6f2d0987883d2b3589f9d2ee2f87490ec4523da9
Etsuko <ejb0515@gmail.com>
2026-06-22 12:33:07 +0900

    Mobile共通デザイントークンを追加 (#1650)

---
+++ apps/mobile/app/design/cardSizes.ts
@@ -0,0 +1,18 @@
+export const cardSizes = {
+  borderWidth: 1,
+  radiusSm: 12,
+  radiusMd: 16,
+  radiusLg: 20,
+  pillRadius: 999,
+  carouselWidth: 220,
+  carouselWidthLg: 256,
+  imageSm: 64,
+  imageRadius: 12,
+  cardPaddingSm: 12,
+  cardPaddingMd: 14,
+  cardPaddingLg: 18,
+  rankBadgeSize: 32,
+  skeletonWidth: 160,
+  skeletonRadius: 14,
+  shimmerRadius: 8,
+} as const;
