881a1af369c76e5be9825fc13c43621b5bc01194
morietsu <ejb0515@gmail.com>
2026-09-26 21:09:57 +0900

    feat(admin): enable user search by username and email

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,6 +6,7 @@ try:
     @admin.register(UserProfile)
     class UserProfileAdmin(admin.ModelAdmin):
         list_display = ("user", "nickname", "is_public", "created_at")
+        search_fields = ("user__username", "user__email")
 
 except Exception:
     # UserProfile が無いプロジェクトでも落ちないようにする
